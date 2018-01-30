bd285bcc37d6e2878ac19c0e7ee954295b84867a
dinhlq <dinhlq@9action.com>
2018-01-30 22:16:52 +0700

    fix bug

--- app/Http/Controllers/v1/Auth/UserController.php
+++ app/Http/Controllers/v1/Auth/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\v1\Auth;
 
 use Illuminate\Support\Facades\Input;
 use Illuminate\Http\Request;
-use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests;
 use JWTAuth;
